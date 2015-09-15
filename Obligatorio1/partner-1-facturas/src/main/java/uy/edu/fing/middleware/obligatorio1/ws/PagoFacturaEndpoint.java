311063a4643f4fbd1925e9b6170249d65c46add0
Farid <farid.elias@gmail.com>
2015-09-15 10:24:36 -0300

    Wrappers eliminados, recibiendo los parámetros directamente en el web service.

--- Obligatorio1/partner-1-facturas/src/main/java/uy/edu/fing/middleware/obligatorio1/ws/PagoFacturaEndpoint.java
+++ Obligatorio1/partner-1-facturas/src/main/java/uy/edu/fing/middleware/obligatorio1/ws/PagoFacturaEndpoint.java
@@ -1,15 +1,18 @@
 package uy.edu.fing.middleware.obligatorio1.ws;
 
 
-import uy.edu.fing.middleware.obligatorio1.ws.domain.PagoFacturaRequest;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
-import uy.edu.fing.middleware.obligatorio1.ws.domain.PagoFacturaLogReply;
-import uy.edu.fing.middleware.obligatorio1.ws.domain.PagoFacturaReply;
+import javax.xml.bind.annotation.XmlElement;
 
 import uy.edu.fing.middleware.obligatorio1.ws.exceptions.FacturaNoEncontrada;
 import uy.edu.fing.middleware.obligatorio1.ws.exceptions.MonedaNoEncontrada;
@@ -25,36 +28,35 @@ public class PagoFacturaEndpoint {
     private final List<String> log = new ArrayList<>();
     
     Date date;
-    
-	//@PayloadRoot(namespace = NAMESPACE_URI, localPart = "pagoFacturaRequest")
-	//@ResponsePayload
-    //
+
     @WebMethod
-    public PagoFacturaReply pagoFactura(PagoFacturaRequest request) throws FacturaNoEncontrada, MonedaNoEncontrada {
-		PagoFacturaReply response = new PagoFacturaReply();
+    public  Integer pagoFactura(@WebParam(name = "facturaID") @XmlElement(required=true) Long facturaID, 
+            @WebParam(name = "monedaID") @XmlElement(required=true) short monedaID, 
+            @WebParam(name = "monto") @XmlElement(required=true) BigDecimal monto, 
+            @WebParam(name = "fecha") @XmlElement(required=true) GregorianCalendar fecha) 
+            throws FacturaNoEncontrada, MonedaNoEncontrada {
         date = new Date();
         String msg = "";
-        log.add(date + ": -->" + request.toString());
-        if(request.getMonedaID() != 1 && request.getMonedaID() != 2){
+        String requestMsg = String.format("facturaID: %d, monedaID: %d, monto: %s, fecha: %tB",facturaID, monedaID, NumberFormat.getCurrencyInstance().format(monto), fecha);
+        log.add(date + ": -->" + requestMsg);
+        if(monedaID!= 1 && monedaID!= 2){
             msg = "Moneda no encontrada";
             log.add(date + ": -->" + msg);
             throw new MonedaNoEncontrada(msg);
         }
-        if(request.getFacturaID() != 666 ){
+        if(facturaID!= 666 ){
             msg = "Factura no encontrada";
             log.add(date + ": -->" + msg);
             throw new FacturaNoEncontrada(msg);
         }
-        response.setCobroID(1942L);
-        log.add(date + ": -->" + response.toString());
+        Integer response = 1942;
+        log.add(date + ": -->" + response);
         return response;
 	}
     
     @WebMethod
-    public PagoFacturaLogReply pagoFacturaLog(){
-        PagoFacturaLogReply response = new PagoFacturaLogReply();
-        response.setLogList(log);
-        return response;
+    public @WebResult(name="log") List<String> pagoFacturaLog(){
+        return log;
     }
 
 }
