6b338ab2b49b742d5fe05db833e09de654c54164
Diego Rodriguez <diegoalejandrorodriguez@gmail.com>
2015-11-03 16:13:16 -0300

    Service publicado con ws-policy ws-addressing

--- Obligatorio2/sistemaCentralPagos/src/main/java/uy/edu/fing/middleware/obligatorio2/ws/endpoint/CompraServiceImpl.java
+++ Obligatorio2/sistemaCentralPagos/src/main/java/uy/edu/fing/middleware/obligatorio2/ws/endpoint/CompraServiceImpl.java
@@ -1,6 +1,9 @@
 package uy.edu.fing.middleware.obligatorio2.ws.endpoint;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlElement;
 
 import uy.edu.fing.middleware.obligatorio2.domain.Orden;
 import uy.edu.fing.middleware.obligatorio2.ws.interfaces.CompraService;
@@ -11,7 +14,8 @@ import uy.edu.fing.middleware.obligatorio2.ws.interfaces.CompraService;
 public class CompraServiceImpl implements CompraService{
 
 	@Override
-	public void compra(Orden orden) {
+	@WebMethod
+	public void compra(@WebParam (name = "ordenRequest")@XmlElement(required=true) Orden orden) {
 		System.out.println(orden); 
 		
 	}
