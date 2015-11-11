f805f4038e850cbb76f347c3d9f63a10a62f2026
Mauricio Vignale <mvignale1987@gmail.com>
2015-11-11 14:21:25 -0300

    interaccion con PagosYa

---
+++ Obligatorio2/sistemaCentralPagos/src/main/java/uy/edu/fing/middleware/obligatorio2/domain/PagoResponse.java
@@ -0,0 +1,16 @@
+package uy.edu.fing.middleware.obligatorio2.domain;
+
+public class PagoResponse {
+	
+	private Long identificadorPago;
+
+	public Long getIdentificadorPago() {
+		return identificadorPago;
+	}
+
+	public void setIdentificadorPago(Long identificadorPago) {
+		this.identificadorPago = identificadorPago;
+	}
+	
+	
+}
--- Obligatorio2/sistemaCentralPagos/src/main/java/uy/edu/fing/middleware/obligatorio2/ws/interfaces/PagosRestService.java
+++ Obligatorio2/sistemaCentralPagos/src/main/java/uy/edu/fing/middleware/obligatorio2/ws/interfaces/PagosRestService.java
@@ -3,11 +3,12 @@ package uy.edu.fing.middleware.obligatorio2.ws.interfaces;
 import javax.jws.WebService;
 
 import uy.edu.fing.middleware.obligatorio2.domain.Pago;
+import uy.edu.fing.middleware.obligatorio2.domain.PagoResponse;
 
 @WebService
 //@Addressing(enabled=true,required=true)
 public interface PagosRestService {
 	
-	public void pagoYa(Pago item);
+	public PagoResponse pagoYa(Pago item);
 
 }
