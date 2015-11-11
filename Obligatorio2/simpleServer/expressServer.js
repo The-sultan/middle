bbd701d8c986b1466adfd625326318be58d8670e
Farid <farid.elias@gmail.com>
2015-11-11 01:15:57 -0300

    Comiteado server básico en node que simplemente acepta post y loguea lo que viene en el body de la request.

---
+++ Obligatorio2/simpleServer/expressServer.js
@@ -0,0 +1,12 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  var body = "";
+  req.on('data', function (chunk) {
+    body += chunk;
+  });
+  req.on('end', function () {
+    console.log(body);
+    res.writeHead(200);
+    res.end("hola!!!");
+  });
+}).listen(8081);
