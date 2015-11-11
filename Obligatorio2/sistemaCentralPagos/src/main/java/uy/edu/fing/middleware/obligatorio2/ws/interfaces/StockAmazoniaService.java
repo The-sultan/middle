017e7fa1f5f9b6184adbd5bf8502e2a917945a3e
Mauricio Vignale <mvignale1987@gmail.com>
2015-11-10 21:29:24 -0300

    transformer en response captura perfectamente lo que viene del servicio, falta que vuelva al consumidor nada mas

--- Obligatorio2/sistemaCentralPagos/src/main/java/uy/edu/fing/middleware/obligatorio2/ws/interfaces/StockAmazoniaService.java
+++ Obligatorio2/sistemaCentralPagos/src/main/java/uy/edu/fing/middleware/obligatorio2/ws/interfaces/StockAmazoniaService.java
@@ -3,6 +3,7 @@ package uy.edu.fing.middleware.obligatorio2.ws.interfaces;
 import javax.jws.WebService;
 
 import uy.edu.fing.middleware.obligatorio2.domain.Item;
+import uy.edu.fing.middleware.obligatorio2.domain.AmazoniaResponse;
 
 @WebService
 //@Addressing(enabled=true,required=true)
