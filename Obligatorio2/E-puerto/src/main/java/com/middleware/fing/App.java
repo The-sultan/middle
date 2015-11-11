80cfc99d0b99dac42d750375961891de3479887c
Mauricio Vignale <mvignale1987@gmail.com>
2015-11-11 19:21:57 -0300

    Epuerto procesa ordenes, correr con mvn exec:java

--- Obligatorio2/E-puerto/src/main/java/com/middleware/fing/App.java
+++ Obligatorio2/E-puerto/src/main/java/com/middleware/fing/App.java
@@ -2,6 +2,7 @@ package com.middleware.fing;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardWatchEventKinds;
@@ -9,14 +10,18 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.beans.BeanWrapperImpl;
 import org.supercsv.cellprocessor.ift.CellProcessor;
 import org.supercsv.io.CsvBeanReader;
+import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanReader;
 import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
 
 import com.middleware.fing.model.Order;
+import com.middleware.fing.model.OrderResult;
 import com.middleware.fing.utils.CSVUtils;
 
 /**
@@ -54,33 +59,60 @@ public class App {
 			myDir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE);
 
 			WatchKey watckKey = watcher.take();
+			while (true) {
+				List<WatchEvent<?>> events = watckKey.pollEvents();
 
-			List<WatchEvent<?>> events = watckKey.pollEvents();
+				for (WatchEvent<?> event : events) {
+					if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+						// NEW CSV DEPLOYED ON FOLDER, PROCESSING...
 
-			for (WatchEvent<?> event : events) {
-				if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
-					// NEW CSV DEPLOYED ON FOLDER, PROCESSING...
+						try {
+							System.out.println("Created: " + event.context().toString());
 
-					try {
-						System.out.println("Created: " + event.context().toString());
+							beanReader = new CsvBeanReader(
+									new FileReader(folderOrderPath + File.separator + event.context().toString()),
+									CsvPreference.STANDARD_PREFERENCE);
+							final String[] header = beanReader.getHeader(true);
+							final CellProcessor[] processors = CSVUtils.getOrderProcessors();
 
-						beanReader = new CsvBeanReader(new FileReader(folderOrderPath+File.separator+event.context().toString()), CsvPreference.STANDARD_PREFERENCE);
-						final String[] header = beanReader.getHeader(true);
-						final CellProcessor[] processors = CSVUtils.getOrderProcessors();
+							Order order;
+							while ((order = beanReader.read(Order.class, header, processors)) != null) {
+								OrderResult res = new OrderResult();
+								res.setIdentificadorCompra(order.getIdentificadorCompra());
+								res.setIdentificadorReserva(UUID.randomUUID().toString());
+								res.setCodigo(0);
+								res.setDescripcion("OK");
+								try {
+									beanWriter = new CsvBeanWriter(
+											new FileWriter(
+													folderConfirmPath + File.separator + event.context().toString()),
+											CsvPreference.STANDARD_PREFERENCE);
 
-						Order order;
-						while ((order = beanReader.read(Order.class, header, processors)) != null) {
-							System.out.println(String.format("lineNo=%s, rowNo=%s, customer=%s",
-									beanReader.getLineNumber(), beanReader.getRowNumber(), order));
-						}
+									final CellProcessor[] confirmProcessors = CSVUtils.getConfirmProcessors();
+									final String[] headerConfirm = CSVUtils.getCSVConfirmHeaders();
 
-					} finally {
-						if (beanReader != null) {
-							beanReader.close();
+									beanWriter.writeHeader(headerConfirm);
+
+									beanWriter.write(res, headerConfirm, confirmProcessors);
+								} finally {
+									if (beanWriter != null) {
+										beanWriter.close();
+									}
+								}
+
+								System.out.println(String.format("lineNo=%s, rowNo=%s, customer=%s",
+										beanReader.getLineNumber(), beanReader.getRowNumber(), order));
+							}
+
+						} finally {
+							if (beanReader != null) {
+								beanReader.close();
+							}
 						}
 					}
 				}
 			}
+
 		} catch (Exception e) {
 			System.out.println("Error: " + e.toString());
 		}
