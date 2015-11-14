a7c5232995fe8851138474b4bc291f268c651ac5
Farid <farid.elias@gmail.com>
2015-11-14 02:32:09 -0300

    Haciendo fallar stock local para producto 5 unicamente.

--- Obligatorio2/stockLocal/src/main/java/uy/edu/fing/middleware/obligatorio2/endpoint/StockLocalEndpoint.java
+++ Obligatorio2/stockLocal/src/main/java/uy/edu/fing/middleware/obligatorio2/endpoint/StockLocalEndpoint.java
@@ -25,7 +25,12 @@ public class StockLocalEndpoint implements StockLocal{
 	public @WebResult(name="reservaProductosResponse") DatoSalida reservaProductos(@WebParam (name = "reservaProductosRequest" ) @XmlElement(required=true) List<DatoReserva> productos) throws Exception {
 		System.out.println("Reservar Productos");
 		DatoSalida ret  = new DatoSalida();
-        ret.setCodigo("CODIGO!");
+        if(productos.get(0).getIdProducto().equals(5L)){
+            ret.setCodigo("Error");
+        }
+        else{
+            ret.setCodigo("OK");
+        }
 		ret.setDescripcion("test");
         ret.setIdReserva(1234L);
 		
