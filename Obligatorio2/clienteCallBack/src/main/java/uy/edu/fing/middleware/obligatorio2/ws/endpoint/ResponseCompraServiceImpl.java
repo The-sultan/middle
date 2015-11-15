f6761e9644fc94d2bc6d2473536772a258210e93
Diego Rodriguez <diegoalejandrorodriguez@gmail.com>
2015-11-14 23:43:04 -0300

    Solucionado el problema con el cliente callBack
    Recordar de setear el replyTO http://localhost:8080/clienteCallBack/services/responseCompra

---
+++ Obligatorio2/clienteCallBack/src/main/java/uy/edu/fing/middleware/obligatorio2/ws/endpoint/ResponseCompraServiceImpl.java
@@ -0,0 +1,26 @@
+package uy.edu.fing.middleware.obligatorio2.ws.endpoint;
+
+import javax.jws.Oneway;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import org.apache.log4j.Logger;
+
+import uy.edu.fing.middleware.obligatorio2.ws.interfaces.ResponseCompraService;
+
+@org.apache.cxf.interceptor.InInterceptors (interceptors = {"org.apache.cxf.interceptor.LoggingInInterceptor" })
+//@WebService(endpointInterface="uy.edu.fing.middleware.obligatorio2.interfaces.ResponseCompraService",
+//			serviceName="ResponseCompraService")
+public class ResponseCompraServiceImpl implements ResponseCompraService{
+
+	private static final Logger logger = Logger.getLogger(ResponseCompraServiceImpl.class);
+	
+
+	@Oneway
+	public void callBack(@WebParam(name="return") String callbackMessage){
+		System.out.println("Recieved callback message " + callbackMessage);
+	}
+	
+	
+
+}
